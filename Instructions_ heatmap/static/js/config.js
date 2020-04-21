da44141a68859c4a6e825c1a25d471532a0ec839
rarchana24 <58632574+rarchana24@users.noreply.github.com>
2020-04-21 18:11:15 -0400

    Add files via upload

---
+++ Instructions_ heatmap/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoicmFyY2hhbmEyNCIsImEiOiJjazhsb3hlNWYwZXVjM2ludzFkNjN1cno1In0.1m9sK2pk6AEqWLQ6oBOgzg";
---
+++ Instructions_ heatmap/static/js/heatmap.js
@@ -0,0 +1,4 @@
+var d = "data/static/allmonth_data.geojson";
+d3.json(d,function(data){
+		console.log(data);
+}) 
